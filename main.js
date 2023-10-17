9e148d2dd0d57f315c98402f57ff22e8bcbfc634
BeulaEvangelin <nicholbe@sheridancollege.ca>
2023-10-17 03:51:01 -0400

    ARRAY CREATED

---
+++ main.js
@@ -0,0 +1,9 @@
+let visitorData = [
+
+    {month: "April", visitors: "900"},
+    {month: "May", visitors: "1100"},
+    {month: "June", visitors: "1500"},
+    {month: "July", visitors: "1400"},
+    {month: "August", visitors: "1780"},
+    {month: "September", visitors: "1950"}
+  ];
